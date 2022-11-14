543240a09a52a2055250a8d657ebf98cd05002f8
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-14 09:38:41 +0000

    LeaPassionGaragiste

--- LeaPassionGaragiste.java
+++ LeaPassionGaragiste.java
@@ -1,4 +1,13 @@
 public class LeaPassionGaragiste {
 
-  
+  public static void main(String[] args) {
+    Renault maRenault1 = new Renault(200, 7000, "Bleue", 1999, 0.9);
+    System.out.println(maRenault1.calculerPrixDeVente());
+    
+    Voiture maRenault2 = new Renault(200, 6000, "Bleue", 1999, 0.9);
+    System.out.println(maRenault2.calculerPrixDeVente());
+    
+    Limousine maLimousine = new Limousine(200, 5500, "Bleue", 1999, 0.9, 10);
+    System.out.println(maLimousine.calculerPrixDeVente());
+  }
 }
